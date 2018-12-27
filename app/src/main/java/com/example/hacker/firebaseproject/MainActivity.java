9e5e9845b20f573638c94aecad4bc0381e678cda
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-12-28 05:21:16 +0800

    first commit

---
+++ app/src/main/java/com/example/hacker/firebaseproject/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.hacker.firebaseproject;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private static final String TAG = "MainActivity";
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private Button signup_Btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Message");
+//        databaseReference.setValue("Hello Firebase");
+        databaseReference.setValue("Firebasen");
+
+//        email = findViewById(R.id.emailET);
+//
+//        password = findViewById(R.id.passwordET);
+//        login = findViewById(R.id.loginBtn);
+
+        Button newActivity = findViewById(R.id.newActivity);
+        newActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(), SecondActivity.class));
+            }
+        });
+
+
+        setupLoginInput();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email_addr  = email.getText().toString();
+                String password_str = password.getText().toString();
+                if (!email_addr.equals("") && !password_str.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email_addr, password_str)
+                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+//                                        updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+//                                        updateUI(null);
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+            }
+        });
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // user is signed in
+                    Log.d(TAG, "user signed in");
+                } else {
+                    // user is signed out
+                    Log.d(TAG, "user signed out");
+                }
+            }
+        };
+    }
+
+    private void setupLoginInput() {
+        email = findViewById(R.id.emailET);
+        password = findViewById(R.id.passwordET);
+        login = findViewById(R.id.loginBtn);
+        signup_Btn = findViewById(R.id.sign_up_Btn);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hacker/firebaseproject/SecondActivity.java
@@ -0,0 +1,63 @@
+package com.example.hacker.firebaseproject;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SecondActivity extends AppCompatActivity {
+    private Button btn1;
+    private Button btn2;
+    private TextView tv;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        tv = findViewById(R.id.text);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Message");
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.setValue("Button1 Clicked");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.setValue("Button2 Clicked");
+
+            }
+        });
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                tv.setText(databaseReference.toString());
+//                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+                String value = dataSnapshot.getValue(String.class);
+                tv.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+}
